9d35ae59ec5002e2171c2ea15b63545973a80a84
vanhalenar <thalenar@gmail.com>
2025-04-03 23:49:27 +0200

    general case working

--- oets.cpp
+++ oets.cpp
@@ -14,25 +14,19 @@ int main(int argc, char *argv[])
 	MPI_Status status;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	// printf("I am %d of %d\n", rank, size);
-
-	// vector<u_int8_t> bytes = getInputData();
 
 	ifstream inputData("numbers", ios::binary);
 
 	inputData.seekg(rank);
 	u_int8_t num;
 	inputData.read(reinterpret_cast<char *>(&num), 1);
-
-	MPI_Barrier(MPI_COMM_WORLD);
+	inputData.close();
 
 	u_int8_t *allNums = nullptr;
 	if (rank == 0)
 	{
 		allNums = new u_int8_t[size];
 	}
-
-	// Gather all bytes into rank 0
 	MPI_Gather(&num, 1, MPI_UINT8_T, allNums, 1, MPI_UINT8_T, 0, MPI_COMM_WORLD);
 
 	if (rank == 0)
@@ -48,75 +42,86 @@ int main(int argc, char *argv[])
 
 	u_int8_t num2;
 
-	for (int k = 1; k < 8; k++)
+	for (int k = 1; k < size; k++)
 	{
-		if (rank == 0 || rank == 2 || rank == 4 || rank == 6)
+		// 0-1, 2-3, 4-5, 6-7
+		// send your number to neighbor
+		for (int i = 0; i <= 2 * (size / 2) - 1; i += 2)
 		{
-			MPI_Send(&num, 1, MPI_UINT8_T, rank + 1, 0, MPI_COMM_WORLD);
+			if (i == rank)
+				MPI_Send(&num, 1, MPI_UINT8_T, rank + 1, 0, MPI_COMM_WORLD);
 		}
-		if (rank == 1 || rank == 3 || rank == 5 || rank == 7)
+		for (int i = 1; i <= 2 * (size / 2) - 1; i += 2)
 		{
-			MPI_Send(&num, 1, MPI_UINT8_T, rank - 1, 0, MPI_COMM_WORLD);
+			if (i == rank)
+				MPI_Send(&num, 1, MPI_UINT8_T, rank - 1, 0, MPI_COMM_WORLD);
 		}
 
-		// MPI_Barrier(MPI_COMM_WORLD); dont think we need this one
-
-		if (rank == 0 || rank == 2 || rank == 4 || rank == 6)
+		// recieve number from neighbor
+		for (int i = 0; i <= 2 * (size / 2) - 1; i += 2)
 		{
-			MPI_Recv(&num2, 1, MPI_UINT8_T, rank + 1, 0, MPI_COMM_WORLD, &status);
-			if (num > num2)
+			if (i == rank)
 			{
-				num = num2;
+				MPI_Recv(&num2, 1, MPI_UINT8_T, rank + 1, 0, MPI_COMM_WORLD, &status);
+				if (num > num2)
+				{
+					num = num2;
+				}
 			}
 		}
-		if (rank == 1 || rank == 3 || rank == 5 || rank == 7)
+		for (int i = 1; i <= 2 * (size / 2) - 1; i += 2)
 		{
-			MPI_Recv(&num2, 1, MPI_UINT8_T, rank - 1, 0, MPI_COMM_WORLD, &status);
-			if (num < num2)
+			if (i == rank)
 			{
-				num = num2;
+				MPI_Recv(&num2, 1, MPI_UINT8_T, rank - 1, 0, MPI_COMM_WORLD, &status);
+				if (num < num2)
+				{
+					num = num2;
+				}
 			}
 		}
 
-		MPI_Barrier(MPI_COMM_WORLD); // not sure
+		// MPI_Barrier(MPI_COMM_WORLD); // not sure
 
-		if (rank == 1 || rank == 3 || rank == 5)
+		// 1-2, 3-4, 5-6
+		// send your number to neighbor
+		for (int i = 1; i < 2 * ((size - 1) / 2); i += 2)
 		{
-			MPI_Send(&num, 1, MPI_UINT8_T, rank + 1, 0, MPI_COMM_WORLD);
+			if (i == rank)
+				MPI_Send(&num, 1, MPI_UINT8_T, rank + 1, 0, MPI_COMM_WORLD);
 		}
-		if (rank == 2 || rank == 4 || rank == 6)
+		for (int i = 2; i <= 2 * ((size - 1) / 2); i += 2)
 		{
-			MPI_Send(&num, 1, MPI_UINT8_T, rank - 1, 0, MPI_COMM_WORLD);
+			if (i == rank)
+				MPI_Send(&num, 1, MPI_UINT8_T, rank - 1, 0, MPI_COMM_WORLD);
 		}
-		if (rank == 1 || rank == 3 || rank == 5)
+
+		// recieve number from neighbor
+		for (int i = 1; i < 2 * ((size - 1) / 2); i += 2)
 		{
-			MPI_Recv(&num2, 1, MPI_UINT8_T, rank + 1, 0, MPI_COMM_WORLD, &status);
-			if (num > num2)
+			if (i == rank)
 			{
-				num = num2;
+				MPI_Recv(&num2, 1, MPI_UINT8_T, rank + 1, 0, MPI_COMM_WORLD, &status);
+				if (num > num2)
+				{
+					num = num2;
+				}
 			}
 		}
-		if (rank == 2 || rank == 4 || rank == 6)
+		for (int i = 2; i <= 2 * ((size - 1) / 2); i += 2)
 		{
-			MPI_Recv(&num2, 1, MPI_UINT8_T, rank - 1, 0, MPI_COMM_WORLD, &status);
-			if (num < num2)
+			if (i == rank)
 			{
-				num = num2;
+				MPI_Recv(&num2, 1, MPI_UINT8_T, rank - 1, 0, MPI_COMM_WORLD, &status);
+				if (num < num2)
+				{
+					num = num2;
+				}
 			}
 		}
 	}
 
 	MPI_Barrier(MPI_COMM_WORLD);
-
-	/* for (int i = 0; i < size; i++)
-	{
-		if (i == rank)
-		{
-			printf("I am %d of %d: %d\n", rank, size, num);
-			fflush(stdout);
-		}
-		MPI_Barrier(MPI_COMM_WORLD);
-	} */
 
 	MPI_Gather(&num, 1, MPI_UINT8_T, allNums, 1, MPI_UINT8_T, 0, MPI_COMM_WORLD);
 
@@ -126,7 +131,6 @@ int main(int argc, char *argv[])
 		{
 			printf("%d\n", (int)allNums[i]);
 		}
-		printf("\n");
 	}
 
 	MPI_Finalize();
